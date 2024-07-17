3345ae8f6530b7590845fc8bac51e42436a6ba4e
Dhruv9512 <dhruvsharma56780@gmail.com>
2024-07-17 17:28:16 +0530

    make api

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
-from flask import Flask, jsonify,render_template
+from flask import Flask, jsonify
 from flask_cors import CORS
 import psycopg2
 import os
 
-
-
-
 app = Flask(__name__)
 CORS(app)
+
+# Configuration from environment variables
 app.config['POSTGRES_HOST'] = os.getenv('DB_HOST')
 app.config['POSTGRES_USER'] = os.getenv('DB_USER')
 app.config['POSTGRES_PASSWORD'] = os.getenv('DB_PASSWORD')
 app.config['POSTGRES_DB'] = os.getenv('DB_NAME')
+
 
 def connect_to_database():
     conn = psycopg2.connect(
@@ -22,46 +22,37 @@ def connect_to_database():
     )
     return conn
 
-@app.route('/home')
-def home():
-    return render_template('home.html')
-@app.route('/contact')
-def home():
-    return render_template('contact.html')
-@app.route('/about')
-def home():
-    return render_template('about.html')
-@app.route('/mywork')
-def home():
-    return render_template('mywork.html')
 
-@app.route('/api', methods=["GET"])
+@app.route('/api', methods=["POST"])
 def api():
-    try:
-        conn = connect_to_database()
-        cur = conn.cursor()
-        cur.execute('SELECT * FROM projects')
-        data = cur.fetchall()
-        cur.close()
-        conn.close()
+    if request.method == "POST":
+        try:
+            conn = connect_to_database()
+            cur = conn.cursor()
+            cur.execute('SELECT * FROM projects')  # Adjust query according to your database structure
+            data = cur.fetchall()
+            cur.close()
+            conn.close()
 
-        # Structure data into JSON format
-        json_data = []
-        for item in data:
-            json_data.append({
-                'Id': item[0],
-                'Title': item[1],
-                'Description': item[2],
-                'GithubLink': item[3],
-                'WebsiteLink': item[4]
-            })
+            # Structure data into JSON format
+            json_data = []
+            for item in data:
+                json_data.append({
+                    'Id': item[0],
+                    'Title': item[1],
+                    'Description': item[2],
+                    'GithubLink': item[3],
+                    'WebsiteLink': item[4]
+                })
 
-        return jsonify(json_data)
+            return jsonify(json_data)
 
-    except Exception as e:
-        return jsonify({'error': str(e)})
+        except Exception as e:
+            return jsonify({'error': str(e)})
+    else:
+        return jsonify({'error': 'Method not allowed'}), 405
 
 
 if __name__ == "__main__":
-    DEBUG_MODE =os.getenv('DEBUG_MODE')=='True'
+    DEBUG_MODE = os.getenv('DEBUG_MODE') == 'True'
     app.run(debug=DEBUG_MODE)
